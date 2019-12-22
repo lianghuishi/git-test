05d59c85c1f9e4ac436782b4307b49986aaeb779
admin <421585364@qq.com>
2019-12-22 21:46:09 +0800

    测试分支

---
+++ src/com/branch1/Main1.java
@@ -0,0 +1,8 @@
+package com.branch1;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/com/branch2/Main2.java
@@ -0,0 +1,8 @@
+package com.branch2;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
